a4c448af2a3710f989ff1a0da8b15b29b21d5ca2
lkri <lkri@techcollege.dk>
2022-11-03 11:17:46 +0100

    Unit test for Card.ToCompare

--- TestOOPUNOExamples/UnitTestCards.cs
+++ TestOOPUNOExamples/UnitTestCards.cs
@@ -8,5 +8,50 @@ namespace TestOOPUNOExamples
 {
     public class UnitTestCards
     {
+        Card redCard1 = new RedCard(1);
+        Card redCard2 = new RedCard(2);
+        Card blueCard1 = new BlueCard(1);
+        Card blueCard2 = new BlueCard(2);
+        Card blueActionCard = new BlueActionCard();
+        Card redActionCard = new RedActionCard();
+        Card wildCard = new WildCard();
+        [Fact]
+        public void TestToCompareColor()
+        {
+            Assert.True(redCard1.ToCompare(redCard2));
+            Assert.True(redCard1.ToCompare(redActionCard));
+            Assert.True(blueCard1.ToCompare(blueCard2));
+            Assert.True(blueCard2.ToCompare(blueActionCard));
+        }
+        [Fact]
+        public void TestToCompareNumber()
+        {
+            Assert.True(redCard1.ToCompare(blueCard1));
+            Assert.True(blueCard2.ToCompare(redCard2));
+        }
+        [Fact]
+        public void TestToCompareType()
+        {
+            Assert.True(blueActionCard.ToCompare(redActionCard));
+            Assert.True(redActionCard.ToCompare(blueActionCard));
+            Assert.True(wildCard.ToCompare(redCard2));
+            Assert.True(redCard1.ToCompare(wildCard));
+        }
+        [Fact]
+        public void TestToCompareWildCard()
+        {
+            Assert.True(wildCard.ToCompare(redActionCard));
+            Assert.True(redActionCard.ToCompare(wildCard));
+            Assert.True(wildCard.ToCompare(redCard2));
+            Assert.True(redCard1.ToCompare(wildCard));
+        }
+        [Fact]
+        public void TestToCompareFails()
+        {
+            Assert.False(blueActionCard.ToCompare(redCard1));
+            Assert.False(redActionCard.ToCompare(blueCard1));
+            Assert.False(blueCard2.ToCompare(redCard1));
+            Assert.False(redCard2.ToCompare(blueCard1));
+        }
     }
 }
